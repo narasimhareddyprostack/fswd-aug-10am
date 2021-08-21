ece8b286d397f83e62b56d8b00e24880faa3c1c2
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-08-21 13:29:41 +0530

    website

--- Block 2/JS-Array/size.js
+++ Block 2/JS-Array/size.js
@@ -1,5 +1,6 @@
-let size = [40, 42, 44];
-let newLength = size.push(46);
-console.log(size);
-console.log(newLength);
-//Push() method - add value end your array and return the new lenght
+    let size = [40, 42, 44];
+    let newLength = size.push(46);
+    console.log(size);
+    console.log(newLength);
+    //Push() method - add value end your array and return the new lenght
+ 
